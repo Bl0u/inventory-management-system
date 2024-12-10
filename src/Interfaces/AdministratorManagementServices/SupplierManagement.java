c83428e5321476097031b41434645519ada52530
Peter Emil <peteremil791@gmail.com>
2024-12-11 00:19:53 +0200

    - SupplierManager HandleXFunctions DONE

--- src/Interfaces/AdministratorManagementServices/SupplierManagement.java
+++ src/Interfaces/AdministratorManagementServices/SupplierManagement.java
@@ -3,10 +3,16 @@ package Interfaces.AdministratorManagementServices;
 import Classes.*;
 import Classes.Products.Product;
 import Classes.Users.Supplier;
+import Classes.Users.User;
 
 public interface SupplierManagement  {
+    public boolean addSupplier ()     ;
+    public boolean updateSupplier ()  ;
+    public boolean deleteSupplier ()  ;
 
-    public boolean reportSupplierPerformance(Supplier supplier) ;
-    public boolean reportSuppliersRelatedToThisProduct(Product product) ;
+    public boolean storeInDB(Supplier supplier);
+    public Supplier findSupplierByEmail(String email);
+    public boolean reportSupplierPerformance() ;
+    public boolean reportSuppliersRelatedToThisProduct() ;
     public void handleSupplierManagement(int supplierChoice);
 }
